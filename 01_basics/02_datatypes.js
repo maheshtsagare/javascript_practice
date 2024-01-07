4852aa316fe6933ca8ddd5dfd42e0b053891f729
maheshtsagare <113876049+maheshtsagare@users.noreply.github.com>
2024-01-07 15:19:41 +0000

    datatypes and conversion

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+console.log(typeof "mahesh");
+console.log(typeof 3);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+/*
+data types
+number
+bigInt
+string
+boolean
+null 
+object
+*/
+
---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,68 @@
+//Conversion Into NUMBER
+
+// let score=45;
+// let score="45abc";
+// let score=null;
+// let score=undefined;
+// let score=true;
+// let score=false;
+// let score="mahesh";
+// let score="";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+// let valueOfscoreInNumber=Number(score);
+// console.log(valueOfscoreInNumber);
+// console.log(typeof valueOfscoreInNumber);
+
+//"45" => 45
+//"45abc" => NaN
+//null => 0
+//undefined => NaN
+//true => 1
+//false => 0
+//"mahesh" => NaN
+//"" =>0
+//** Note: type of NaN is number
+
+
+//Conversion into BOOLEAN
+// let isLoggedIn=1;
+// let isLoggedIn=0;
+// let isLoggedIn= "";
+// let isLoggedIn="mahesh";
+// let isLoggedIn=null;
+// let isLoggedIn=undefined;
+
+// let valueOfisLoggedInInBoolean=Boolean(isLoggedIn)
+// console.log(valueOfisLoggedInInBoolean);
+// console.log(typeof valueOfisLoggedInInBoolean);
+
+//1 => true
+//0 => false
+//"" => false
+//"mahesh" => true
+//null => 0
+//undefined => false
+    
+
+//Conversion into STRING
+
+
+// let number=1;
+// let number=true;
+// let number=false;
+// let number=null;
+// let number=undefined;
+
+let valueOfNumberIntoString=String(number)
+console.log(valueOfNumberIntoString);
+console.log(typeof valueOfNumberIntoString);
+
+// 1 => 1 as a string
+// true => true as a string
+// false => false as a string
+// null => null as a string
+// undefined => undefined as a string
